cfd3af332aed21bfadda40610fb9e2944df37519
Eric Oliveira <ericdesenvolvedor2003@gmail.com>
2023-08-05 20:50:21 -0300

    feat: Criando resposta em Json.

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World</h1>');
 });
 
+app.get('/json', (resquest, response) => {
+  response.json({
+    id: 1,
+    title: 'Tarefa 1',
+    done: false
+  })
+})
+
 app.listen(3000, () => console.log('Escutando na porta 3000.'));
